52e1c9983bac6ced2b1e350919242022b9f462ac
pe3-k <59253152+pe3-k@users.noreply.github.com>
2021-06-02 09:34:24 +0200

    Add files via upload
    upload files

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("nav, .about-me h1, .about-me p").hide();
+  $("nav, .about-me h1, .about-me p").fadeIn(1000);
+  $("#logo").hide();
+  $("#logo").fadeIn(1000);
+  $(".hero h1").hide();
+  $(".hero h1").fadeIn(1000);
+  $(".hero p").hide();
+  $(".hero p").fadeIn(1000);
+})
+
